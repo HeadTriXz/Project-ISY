8f5010117eeae11a846ec37e8384a4f74c3efe23
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-11-14 16:42:13 +0100

    JavaDocs for GUI

--- src/main/java/com/headtrixz/ui/TournamentSettingController.java
+++ src/main/java/com/headtrixz/ui/TournamentSettingController.java
@@ -12,19 +12,20 @@ public class TournamentSettingController {
     @FXML
     private TextField portField;
 
+    /**
+     * FXML init method. Makes sure that the settings are persistence between
+     * launches.
+     */
     public void initialize() {
         usernameField.setText(UIManager.getSetting("username"));
         ipField.setText(UIManager.getSetting("ip"));
         portField.setText(UIManager.getSetting("port"));
     }
 
-    private void save() {
-        UIManager.setSetting("username", usernameField.getText());
-        UIManager.setSetting("ip", ipField.getText());
-        UIManager.setSetting("port", portField.getText());
-    }
-
-    public void connect() throws NumberFormatException {
+    /**
+     * Makes the connection the the server.
+     */
+    private void connect() {
         Connection conn = Connection.getInstance();
 
         try {
@@ -34,22 +35,35 @@ public class TournamentSettingController {
         }
     }
 
+    /**
+     * On click event for when the back home button is pressed.
+     */
     public void back() {
         saveAndSwitch("home");
     }
 
+    /**
+     * On click event for when the TTT button is pressed.
+     */
     public void playTicTacToe() {
         connect();
         // TODO: response when connected or not!
         saveAndSwitch("tournament");
     }
 
+    /**
+     * On click event for when the Othello button is pressed.
+     */
     public void playOthello() {
         connect();
         // TODO: response when connected or not!
         // saveAndSwitch("othello");
     }
 
+    /**
+     * Saves all the settings and switches from screen.
+     * @param name The screen to switch to.
+     */
     private void saveAndSwitch(String name) {
         UIManager.setSetting("username", usernameField.getText());
         UIManager.setSetting("ip", ipField.getText());
@@ -58,6 +72,10 @@ public class TournamentSettingController {
         UIManager.switchScreen(name);
     }
 
+    /**
+     * Validates all the input fields.
+     * @return true when the values seem all right, false when they don't
+     */
     private boolean validate() {
         // TODO: validate fields of ui
         return true;
