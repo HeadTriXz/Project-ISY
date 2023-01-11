1c415a49be733dc08672c3b2f5a38f9b4b601d6c
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2023-01-11 23:28:20 +0100

    Javadocs

--- src/main/java/com/headtrixz/factory/MiniMaxFactory.java
+++ src/main/java/com/headtrixz/factory/MiniMaxFactory.java
@@ -11,12 +11,20 @@ import com.headtrixz.minimax.NegaMax;
  */
 public class MiniMaxFactory {
     /**
-     * The different types of MiniMax
+     * The different types of MiniMax.
      */
     public enum MiniMaxType {
         NegaMax, MiniMaxGPU, MiniMax
     }
 
+    /**
+     * Create a new MiniMax instance.
+     *
+     * @param type The specific algorithm to use.
+     * @param game a link to the game.
+     * @param player the machine.
+     * @return a fresh minimax instance.
+     */
     public static MiniMax createMiniMax(MiniMaxType type, GameModel game, Player player) {
         return switch (type) {
             case NegaMax -> new NegaMax(game);
--- src/main/java/com/headtrixz/minimax/BasicMiniMax.java
+++ src/main/java/com/headtrixz/minimax/BasicMiniMax.java
@@ -4,20 +4,34 @@ import com.headtrixz.game.GameBoard;
 import com.headtrixz.game.GameModel;
 import com.headtrixz.game.players.Player;
 
+/**
+ * Basic Minimax is a recreation of a simple version of the minimax algorithm.
+ */
 public class BasicMiniMax implements MiniMax {
     private final GameModel baseGame;
     private final Player maxPlayer;
 
+    /**
+     * Create a new BasicMiniMax object.
+     */
     public BasicMiniMax(GameModel game, Player player) {
         this.baseGame = game;
         this.maxPlayer = player;
     }
 
+    /**
+     * Returns the best move the current player can play based on the current game state.
+     */
     @Override
     public int getMove() {
         return getMove(baseGame.getValidMoves().size());
     }
 
+    /**
+     * Returns the best move the current player can play based on the current game state.
+     *
+     * @param maxDepth the max depth the algorithm should search.
+     */
     @Override
     public int getMove(int maxDepth) {
         GameModel clone = baseGame.clone();
@@ -40,6 +54,14 @@ public class BasicMiniMax implements MiniMax {
         return bestMove;
     }
 
+    /**
+     * The minimax algorithm.
+     *
+     * @param game Current game state.
+     * @param depth The max depth the algorithm should search.
+     * @param player The player for who to search.
+     * @return The best (or worst) value of any board.
+     */
     private int minimax(GameModel game, int depth, Player player) {
         if (depth == 0 || game.getState() != GameModel.GameState.PLAYING) {
             return game.getScore(maxPlayer, depth);
@@ -52,6 +74,15 @@ public class BasicMiniMax implements MiniMax {
         return min(game, depth, player);
     }
 
+
+    /**
+     * The maximize calculations.
+     *
+     * @param game The current game state.
+     * @param depth The depth it is allowed to travel.
+     * @param player The current player.
+     * @return The max value.
+     */
     private int max(GameModel game, int depth, Player player) {
         Player opponent = game.getOpponent(player);
         int value = Integer.MIN_VALUE;
@@ -65,6 +96,14 @@ public class BasicMiniMax implements MiniMax {
         return value;
     }
 
+    /**
+     * The minimize calculations.
+     *
+     * @param game The current game state.
+     * @param depth The depth it is allowed to travel.
+     * @param player The current player.
+     * @return The min value.
+     */
     private int min(GameModel game, int depth, Player player) {
         Player opponent = game.getOpponent(player);
         int value = Integer.MAX_VALUE;
