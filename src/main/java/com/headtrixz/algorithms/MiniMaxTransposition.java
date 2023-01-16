3d5aeeb019fa950eb81f266eb4e1f40c7f01db2c
Rick <r.stoit@gmail.com>
2023-01-16 21:18:12 +0100

    Implemented TranspositionTable

---
+++ src/main/java/com/headtrixz/algorithms/MiniMaxTransposition.java
@@ -0,0 +1,96 @@
+package com.headtrixz.algorithms;
+
+import com.headtrixz.game.GameModel;
+import com.headtrixz.game.players.Player;
+import java.util.Arrays;
+
+/**
+ * Basic Minimax is a recreation of a simple version of the minimax algorithm.
+ */
+public class MiniMaxTransposition implements MiniMax {
+    private final GameModel baseGame;
+    private final TranspositionTable transpositionTable = TranspositionTable.getInstance();
+
+    /**
+     * Create a new BasicMiniMax object.
+     */
+    public MiniMaxTransposition(GameModel game) {
+        this.baseGame = game;
+    }
+
+    /**
+     * Returns the best move the current player can play based on the current game state.
+     */
+    @Override
+    public int getMove() {
+        return getMove(baseGame.getBoard().getCellCount());
+    }
+
+    /**
+     * Returns the best move the current player can play based on the current game state.
+     *
+     * @param maxDepth the max depth the algorithm should search.
+     */
+    @Override
+    public int getMove(int maxDepth) {
+        Player maxPlayer = baseGame.getCurrentPlayer();
+        Player minPlayer = baseGame.getOpponent(maxPlayer);
+        String gameAsString = Arrays.toString(baseGame.getBoard().getCells());
+
+        int bestMove = -1;
+        int value = transpositionTable.get(gameAsString);
+        if (value != Integer.MIN_VALUE) {
+            return value;
+        }
+
+        for (int move : baseGame.getValidMoves()) {
+            GameModel clone = baseGame.clone();
+            clone.setMove(move, maxPlayer.getId());
+
+            int score = minimax(clone, maxDepth, minPlayer);
+            if (score > value) {
+                value = score;
+                bestMove = move;
+            }
+        }
+
+        transpositionTable.put(gameAsString, bestMove);
+
+
+        return bestMove;
+    }
+
+    /**
+     * The minimax algorithm.
+     *
+     * @param game   Current game state.
+     * @param depth  The max depth the algorithm should search.
+     * @param player The player for who to search.
+     * @return The best (or worst) value of any board.
+     */
+    private int minimax(GameModel game, int depth, Player player) {
+        game.setCurrentPlayer(player);
+
+        Player maxPlayer = baseGame.getCurrentPlayer();
+        if (depth == 0 || game.getState() != GameModel.GameState.PLAYING) {
+            return game.getScore(maxPlayer, depth);
+        }
+
+        int maxScore = player == maxPlayer
+            ? Integer.MIN_VALUE
+            : Integer.MAX_VALUE;
+
+        Player opponent = game.getOpponent(player);
+        for (int move : game.getValidMoves()) {
+            GameModel clone = game.clone();
+            clone.setMove(move, player.getId());
+
+            int score = minimax(clone, depth - 1, opponent);
+            maxScore = player == maxPlayer
+                ? Math.max(maxScore, score)
+                : Math.min(maxScore, score);
+        }
+
+        return maxScore;
+    }
+}
--- src/main/java/com/headtrixz/factory/MiniMaxFactory.java
+++ src/main/java/com/headtrixz/factory/MiniMaxFactory.java
@@ -3,6 +3,7 @@ package com.headtrixz.factory;
 import com.headtrixz.algorithms.BasicMiniMax;
 import com.headtrixz.algorithms.MiniMax;
 import com.headtrixz.algorithms.MiniMaxAlphaBeta;
+import com.headtrixz.algorithms.MiniMaxTransposition;
 import com.headtrixz.algorithms.NegaMax;
 import com.headtrixz.game.GameModel;
 
@@ -33,6 +34,7 @@ public class MiniMaxFactory {
             case NegaMax -> new NegaMax(game);
             case MiniMax -> new BasicMiniMax(game);
             case MiniMaxAlphaBeta -> new MiniMaxAlphaBeta(game);
+            case MiniMaxTransposition -> new MiniMaxTransposition(game);
             default -> throw new RuntimeException("Algorithm is not implemented");
         };
     }
